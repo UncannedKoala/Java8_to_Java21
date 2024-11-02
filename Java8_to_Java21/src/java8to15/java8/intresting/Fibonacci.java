e19e4424c599ccf2f972cf3d8f4d44201e1b1673
Piyush Lovanshi <piyush.lovanshi@gmail.com>
2024-11-02 13:44:41 +0530

    spelling corrections

--- Java8_to_Java21/src/java8to15/java8/intresting/Fibonacci.java
+++ Java8_to_Java21/src/java8to15/java8/intresting/Fibonacci.java
@@ -4,16 +4,16 @@ import java.util.stream.Stream;
 
 public class Fibonacci {
 	public static void main(String[] args) {
-//		fabinacciTuples(30);
-		fabinacciSeries(30);		
+		fibonacciTuples(30);
+		fibonacciSeries(30);		
 	}
 
-	public static void fabinacciTuples(int target) {
+	public static void fibonacciTuples(int target) {
 		Stream.iterate(new int[] { 0, 1 }, arr -> new int[] { arr[1], arr[0] + arr[1] }).limit(target)
 				.forEach(tuple -> System.out.println("[" + tuple[0] + ", " + tuple[1] + "]"));
 	}
 
-	public static void fabinacciSeries(int target) {
+	public static void fibonacciSeries(int target) {
 		Stream.iterate(new int[] { 0, 1 }, arr -> new int[] { arr[1], arr[0] + arr[1] }).limit(target)
 		.forEach(tuple -> System.out.print(tuple[0]+", "));
 	}
